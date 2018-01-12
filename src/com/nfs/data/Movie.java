02f451702d00fbff84ccd9ea35701e70f7e0bcfd
Robin La Rondelle <rarondel@avans.nl>
2018-01-12 15:49:03 +0100

    Movie push

--- src/com/nfs/data/Movie.java
+++ src/com/nfs/data/Movie.java
@@ -1,9 +1,9 @@
 package com.nfs.data;
 
 import com.nfs.connections.DatabaseConnection;
-import com.nfs.userinterface.InterfaceCreator;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class Movie {
@@ -14,72 +14,17 @@ public class Movie {
 
 
     //Constructor
-    public Movie()   {
+    public Movie() {
         this.dbc = new DatabaseConnection();
 //        this.movies = new ArrayList<>();
     }
 
-
-    public JPanel getInformationAboutFilm(String movieName) {
-
-        //BUG BELOW
-//        //Declarations
-//        String query =  "SELECT * " +
-//                        "FROM Movie " +
-//                        "WHERE Titel = '" + movieName + "';";
-//        ResultSet rs = dbc.getData(query);
-
-        //Testing purposes
-        String title = "Breaking Bad";
-        String genre = "Avontuur";
-        String language = "Engels";
-        int age = 12;
-        int duration = 136;
-
-        //BUG BELOW
-//        //Retrieving and sorting all the data
-//        try {
-//            title = rs.getString("Titel");
-//            genre = rs.getString("Genre");
-//            language = rs.getString("SpokenLanguage");
-//            age = rs.getInt("Age");
-//            duration = rs.getInt("LengthInSeconds");
-//        }
-//
-//        catch (Exception e)   {
-//            e.printStackTrace();
-//        }
-
-        return new InterfaceCreator().createFilmPanel(title, genre, language, age, duration);
+    public JComponent createFilmButtons()  {
+        return new JLabel("Film Buttons");
     }
-
-    //UNNECESSARY METHOD
-//    //Gives all the movies and their corresponding titles
-//    public List<String> getAllFilmsInDB() {
-//
-//        //Declarations
-//        String query =  "SELECT Titel \n" +
-//                        "FROM Movie";
-//        ResultSet rs = dbc.getData(query);
-//
-//        try {
-//
-//            //First, check for an empty ResultSet
-//            if (rs == null) {
-//                throw new IllegalStateException("The ResultSet was empty");
-//            }
-//
-//            else {
-//                //Adding every value
-//                while (rs.next()) {
-//                    movies.add(rs.getString("Titel"));
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return this.movies;
-//    }
 }
+
+
+
+
+
