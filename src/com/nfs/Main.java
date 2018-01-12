4634e57c1ed5199be2c8125ca1fcbf6b2fe241cf
Robin La Rondelle <rarondel@avans.nl>
2018-01-12 13:26:01 +0100

    Side buttons toegevoegd

--- src/com/nfs/Main.java
+++ src/com/nfs/Main.java
@@ -1,11 +1,8 @@
 package com.nfs;
-
-import com.nfs.login.LoginScherm;
 import com.nfs.userinterface.UI;
 
 public class Main {
     public static void main(String[] args) {
-        //LoginScherm.buildUI();
         UI ui = new UI();
         ui.run();
     }
